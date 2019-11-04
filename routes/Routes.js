5b43abf0fc830048f63b4073ded546dcff3f23ae
Simon McCorkindale <simon@funx.jp>
2019-11-04 13:21:14 +0900

    Setup proxy

--- routes/Routes.js
+++ routes/Routes.js
@@ -4,31 +4,31 @@ const todoList = require('../controllers/Controller');
 module.exports = function(app) {
  
   //GET route for version
-  app.route('/api/Version')
+  app.route('/Version')
   	.get(todoList.list_version);
   
   //GET route for sizes
-  app.route('/api/sizes')
+  app.route('/sizes')
     .get(todoList.sizes);
 
   //GET for the running the pseudo instance
-  app.route('/api/pseudo')
+  app.route('/pseudo')
     .get(todoList.psuedo);
 
   //POST for the setting own entropy
-  app.route('/api/setentropy')
+  app.route('/setentropy')
     .post(todoList.setentropy);
 
   //GET for getting entropy
-  app.route('/api/entropy')
+  app.route('/entropy')
     .get(todoList.entropy);
 
   //GET for getting pool
-  app.route('/api/pool')
+  app.route('/pool')
     .get(todoList.getPool);
 
   //GET for getting attractors
-  app.route('/api/attractors')
+  app.route('/attractors')
     .get(todoList.attractors);
 
 };
